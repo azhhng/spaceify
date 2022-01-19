9eaeb1db27a01f9d607dc342bcb4f28894f703f8
azhhng <azhhng@gmail.com>
2022-01-19 16:50:07 -0500

    added link to github

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,6 +11,7 @@ function Header() {
             <h1><a href="/">Spaceify.</a></h1>
             <h2>Hover, click, and enjoy.</h2>
             <h2>Made by <a href="https://alicez.herokuapp.com">Alice Zhang</a>.</h2>
+            <h2>You can find the Github <a href="https://github.com/azhhng/spaceify">here</a>.</h2>
             <button id="game-button" onClick={() => playGame()}>Play Game --&#62;</button>
         </div>
     )
